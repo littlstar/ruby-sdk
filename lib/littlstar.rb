1b77ebc868adce2b1eb45f4f660f93331c314e82
Dominic Giglio <humanshell@gmail.com>
2015-07-10 10:25:08 -0400

    added module level instance vars and methods

--- lib/littlstar.rb
+++ lib/littlstar.rb
@@ -1,5 +1,57 @@
+# Littlstar Ruby Library
+require 'net/http'
+require 'uri'
+require 'json'
 require 'littlstar/version'
 
 module Littlstar
-  # Your code goes here...
+
+  @@apikey   = ''
+  @@api_base = 'https://littlstar.com/api/v1'
+
+  def self.apikey()
+    @@apikey
+  end
+
+  def self.apikey=(key)
+    @@apikey = key
+  end
+
+  def self.api_base()
+    @@api_base
+  end
+
+  def self.api_base=(base)
+    @@api_base = base
+  end
+
+  def self.api_url(path='')
+    api_base + path
+  end
+
+  ##
+  # The request() method is the heart and soul of the library. It is responsible for taking the
+  # connection details from each resource class and creating the Net::HTTP object that will
+  # ultimately send the appropriate request and return the response from the Littstar API server.
+  def self.request(method, path, payload={}, headers={})
+
+    # prepare headers
+    headers = {
+      'User-Agent'   => "ruby-sdk/#{Littlstar::VERSION}",
+      'Content-Type' => 'application/json',
+      'X-Apikey'     => self.apikey
+    }.merge(headers)
+
+    # create the http connection object
+    uri  = URI.parse(self.api_url(path))
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+
+    # open our HTTP connection and send the appropriate request type
+    # the block will automatically close our connection so we don't have to
+    http.start do |connection|
+      response = connection.send_request(method, uri.path, nil, headers)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
 end
